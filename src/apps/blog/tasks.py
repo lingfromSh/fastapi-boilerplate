7e526faddbef104d4405df2f40f921ff0cf23550
Stephen Ling <lingfromsh@163.com>
2024-12-25 17:04:35 +0800

    feat: add apprise

--- src/apps/blog/tasks.py
+++ src/apps/blog/tasks.py
@@ -1,6 +1,15 @@
+from apprise import Apprise
+
 from core.taskiq import broker
 
 
 @broker.task
 async def send_notification_email(email: str) -> None:
     print(f"Sending notification email to {email}")
+
+    apprise = Apprise()
+    apprise.add("mailto://test@example.com")
+    await apprise.async_notify(
+        title="Test",
+        body="Hello, world!",
+    )
